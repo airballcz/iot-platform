3fe86583199b28337867657a915dafd87dfd9039
zhe chen <chenzhe1003@gmail.com>
2017-09-04 17:18:04 +0800

    modify

--- iot-server/src/main/java/org/opensource/demo/iot/server/Application.java
+++ iot-server/src/main/java/org/opensource/demo/iot/server/Application.java
@@ -1,7 +1,16 @@
 package org.opensource.demo.iot.server;
 
 /**
+ * TCP消息处理Handler
+ *
  * Created by zchen@idelan.cn on 2017/9/4.
  */
 public class Application {
+
+
+
+    public static void main(String[] args) {
+
+    }
+
 }
---
+++ iot-server/src/main/java/org/opensource/demo/iot/server/handler/AuthHandler.java
@@ -0,0 +1,9 @@
+package org.opensource.demo.iot.server.handler;
+
+/**
+ * 认证Handler
+ *
+ * Created by zchen@idelan.cn on 2017/9/4.
+ */
+public class AuthHandler {
+}
