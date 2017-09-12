49b9d107529f528a64d84ba4a46d5cd9f5e6d49a
zhe chen <chenzhe1003@gmail.com>
2017-09-12 17:39:53 +0800

    delete file

--- iot-server/src/main/java/org/opensource/demo/iot/server/handler/AuthHandler.java
+++
@@ -1,9 +0,0 @@
-package org.opensource.demo.iot.server.handler;
-
-/**
- * 认证Handler
- *
- * Created by zchen@idelan.cn on 2017/9/4.
- */
-public class AuthHandler {
-}
--- iot-server/src/main/java/org/opensource/demo/iot/server/handler/TcpHandler.java
+++
@@ -1,7 +0,0 @@
-package org.opensource.demo.iot.server.handler;
-
-/**
- * Created by zchen@idelan.cn on 2017/9/4.
- */
-public class TcpHandler {
-}
